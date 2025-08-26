8fc69dc7ebbb753cd09f26ee936c4280c588324b
lushant <lushantshrestha@gmail.com>
2025-08-26 14:27:30 +1000

    updated footer.php to incude more links
    This provides quick links to the social platforms as well as major webpages

--- footer.php
+++ footer.php
@@ -1,6 +1,34 @@
-        <div class="social-links">
-            <a href="#"><img src="images/facebook.png" alt="Facebook"></a>
-            <a href="#"><img src="images/twitter.png" alt="Twitter"></a>
-            <a href="#"><img src="images/instagram.png" alt="Instagram"></a>
-             <br><br>
-        <?php echo date("Y"); ?> &copy; The Clothing Store. All rights reserved.
+        
+
+<footer class="footer">
+  <div class="footer-container">
+
+    <div class="footer-section">
+      <h3>About Us</h3>
+      <p>We provide the best clothing and fashion trends online.</p>
+    </div>
+        <br>
+    <div class="footer-section">
+      <h3>Quick Links</h3>
+      
+        <a href="#">Home</a>  |
+        <a href="#">Shop</a>  |
+        <a href="#">About</a> |
+        <a href="#">Contact</a>
+      
+    </div>
+    <br>
+    <div class="social-links">
+      <h3>Follow Us</h3>
+      <br>
+        <a href="#"><img src="images/facebook.png" alt="Facebook"></a>
+        <a href="#"><img src="images/twitter.png" alt="Twitter"></a>
+        <a href="#"><img src="images/instagram.png" alt="Instagram"></a>
+      
+    </div>
+  </div>
+  
+  <div class="footer-bottom">
+    <?php echo date("Y"); ?> &copy; The Clothing Store. All rights reserved.
+  </div>
+</footer>

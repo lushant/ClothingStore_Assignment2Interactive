30339aed361f2cc8e36c1221b22d852654faf6f8
lushant <lushantshrestha@gmail.com>
2025-08-26 14:21:34 +1000

    Created a contact_us page
    This is for the customer to send messages

---
+++ contact_us.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us - Clothing Store</title>
+    <link rel="stylesheet" href="css/styles.css">
+    
+</head>
+<body>
+
+<header>
+ <?php include 'nav.php'; ?>
+</header>
+
+<?php include   'header.php';  ?>
+
+<section class="form-section">
+<div class="login-container">
+  <form class="login-form">
+    <h2>Contact Us</h2>
+    <div class="input-group">
+      <label for="fullname">Name</label>
+      <input type="text" id="fullname" name="fullname" placeholder="Enter your username" required>
+    </div>
+    <div class="input-group">
+      <label for="email">Email</label>
+      <input type="text" id="email" name="email" placeholder="Enter your email" required>
+    </div>
+        <div class="input-group">
+      <label for="message">Message</label>
+      <textarea id="message" name="message" rows = "6" cols = "40" placeholder="Enter your message here" required></textarea>
+    </div>
+    <button type="submit">Submit</button>
+     </form>
+</div>
+
+</section>
+<footer>
+    <?php include 'footer.php'; ?>
+</footer>
+</body>
+</html>

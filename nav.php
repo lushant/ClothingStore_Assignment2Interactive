e1d3dcf024616e78a39d8eb1d989a15ca3f3b7c7
lushant <lushantshrestha@gmail.com>
2025-08-26 14:31:36 +1000

    updated navigation for home and contact us

--- nav.php
+++ nav.php
@@ -7,12 +7,12 @@
          <nav>
             <div class="user-menu">
                 <ul>
-                    <li><a href="#">HOME</a></li>
+                    <li><a href="index.php">HOME</a></li>
                     <li><a href="#">MEN</a></li>
                     <li><a href="#">WOMEN</a></li>
                     <li><a href="#">ACCESSORIES</a></li>
                     <li><a href="#">ABOUT US</a></li>
-                    <li><a href="#">CONTACT US</a></li>
+                    <li><a href="contact_us.php">CONTACT US</a></li>
                     <li><a href="#">SIGN UP</a></li>
                     <li><a href="login.php">LOGIN</a></li>
                     <li><a href="#">CART(0)</a></li>

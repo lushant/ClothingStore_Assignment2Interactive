60eddd5dee67c94d238f3850a8b872a74f216356
lushant <lushantshrestha@gmail.com>
2025-09-28 11:26:22 +1000

    Update nav.php

--- nav.php
+++ nav.php
@@ -10,8 +10,7 @@
             <div class="user-menu">
                 <ul>
                     <li><a href="index.php">HOME</a></li>
-                    <li><a href="#">MEN</a></li>
-                    <li><a href="#">WOMEN</a></li>
+                    <li><a href="product.php">SHOP PRODUCT</a></li>
                     <li><a href="#">ACCESSORIES</a></li>
                     <li><a href="#">ABOUT US</a></li>
                     <li><a href="contact_us.php">CONTACT US</a></li>
@@ -24,9 +23,13 @@
 
         </nav>
         <script>    
+        // Highlight active page in navigation
+        // Get current path and filename
             const currentLocation = window.location.pathname.split("/").pop();
             const menuItems = document.querySelectorAll(".user-menu ul a");
-
+        // Loop through menu items to find a match
+        // If match found, add 'active' class
+        // This highlights the current page in the navigation menu
             menuItems.forEach(link => {
                 if(link.getAttribute("href") === currentLocation){
                 link.classList.add("active");
